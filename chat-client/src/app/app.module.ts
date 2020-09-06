90d25f54d99bdeb3b7f63e26d5e9801eee53a617
Nikita27142914 <Nikita27142914@yandex.ru>
2020-09-06 12:43:24 +0300

    add: angular material

--- chat-client/src/app/app.module.ts
+++ chat-client/src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser"
+import { NgModule } from "@angular/core"
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module"
+import { AppComponent } from "./app.component"
+import { LoginComponent } from "./login/login.component"
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
+import { MatSliderModule } from "@angular/material/slider"
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSliderModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
