8d1103d527d08d8e35c5375a39daec3b10efb782
Nikita27142914 <Nikita27142914@yandex.ru>
2020-09-21 11:12:28 +0300

    fix: socket controll

--- chat-client/src/app/app.module.ts
+++ chat-client/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from "./app-routing.module"
 import { AppComponent } from "./app.component"
 import { LoginComponent } from "./login/login.component"
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
-import { MatSliderModule } from "@angular/material/slider"
+import { MatSliderModule } from "@angular/material/slider";
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
